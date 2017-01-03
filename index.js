c2c546e3f9f7495e6488f3ac336fbc8cae490568
Dhruv Patel <dhruv.patel.consultant@nielsen.com>
2017-01-03 17:39:34 +0530

    added comments to the code

--- index.js
+++ index.js
@@ -2,19 +2,26 @@ var http = require('http');
 var call = {};
 
 call.request = function(options) {
+
+  // Same options those are being use in NodeJS http module -
+  // Refer - http://nodejs.org/api/http.html#http_http_request_options_callback
+
   try {
     return new Promise((resolve, reject) => {
       http.request(options, (res) => {
         var data = '';
 
+        // Collect the data via stream
         res.on('data', (chunk) => {
           data += chunk;
         });
 
+        // When stream ends
         res.on('end', () => {
           resolve(data);
         });
 
+        // When error occured
         res.on('error', (e) => {
           reject(`${e.message}`);
         });
